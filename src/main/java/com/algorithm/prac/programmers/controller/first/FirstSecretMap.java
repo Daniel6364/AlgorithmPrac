503fb2024a461552cb0dde8c99a2c235561e50bc
sunghwan Kim <iamsung33@neighbor21.co.kr>
2021-03-31 17:55:22 +0900

    20210331 work

--- src/main/java/com/algorithm/prac/programmers/controller/first/FirstSecretMap.java
+++ src/main/java/com/algorithm/prac/programmers/controller/first/FirstSecretMap.java
@@ -52,10 +52,18 @@ public class FirstSecretMap {
 
     public static String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
+
+        String binaryString = Integer.toBinaryString(11);
+        System.out.println("binaryString : " + binaryString);
+
         return answer;
     }
 
     public static void main(String[] args) {
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
 
+        solution(n, arr1, arr2);
     }
 }
