033ec9f7b0bdc57060fd63129d612927ecfaecb3
sunghwan Kim <iamsung33@neighbor21.co.kr>
2021-05-07 12:52:32 +0900

    210507 commit;

---
+++ src/main/java/com/algorithm/prac/programmers/controller/third/NQueen.java
@@ -0,0 +1,22 @@
+package com.algorithm.prac.programmers.controller.third;
+
+public class NQueen {
+    
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        
+        int n = 4;
+        solution(n);
+
+    }
+
+}
